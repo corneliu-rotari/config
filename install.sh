1545f57e4831cacbf6b96a439c5fd49422bdab2a
Rotari Corneliu <corneliu.rotari99@gmail.com>
2023-03-11 21:50:17 +0200

    Add instalation for oh-my-zsh and oh-my-zsh config files

--- install.sh
+++ install.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 
 # install dependencies configuration
+echo "\n\n====[Update system and installing dependencies]====\n\n"
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt update
 sudo apt install git zsh curl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+nvm install --lts
+
+
+# Here should be the instalation of the nerd fonts
+read -p "\n\nInstalled nerd_fonts [yes/no] : " nerd_fonts
+
+if [ $nerd_fonts = "no" ]; then
+  echo "\nPlease install them at https://www.nerdfonts.com/"
+  exit 0
+fi
 
 # install terminal
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp .zshrc ~
+source ~/.zshrc
+cp -r oh-my-zsh ~/.config
+
+
+(cd $ZSH_CUSTOM/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting)
+(cd $ZSH_CUSTOM/plugins && git clone https://github.com/zsh-users/zsh-autosuggestions)
